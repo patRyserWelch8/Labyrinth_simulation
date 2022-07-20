fa37980facd506b8280a9480ee3e5cd316f80658
Patricia Ryser-Welch <patricia.ryser-welch@newcastle.ac.uk>
2022-07-20 19:33:53 +0100

    testing compass

--- New_labyrinth/labyrinth_solver/labyrinth.py
+++ New_labyrinth/labyrinth_solver/labyrinth.py
@@ -1,10 +1,59 @@
+from labyrinth_solver.agent import Agent
 
 
 class Labyrinth:
-    def __init__(self, aLabyrinth):
-        self.labyrinth  = aLabyrinth
+    labyrinth: object
+
+    def __init__(self, aLabyrinth: object, someEntryPoints: object, someExitPoints: object) -> None:
+        self.labyrinth = aLabyrinth
+        self.entry_points = someEntryPoints
+        self.exit_points = someExitPoints
+        self._boundaries = [0, len(self.labyrinth[0]), 0, len(self.labyrinth)]
+        self.set_cell_values(self.entry_points[1], self.exit_points[0], 2)
+        print(self.labyrinth)
 
     @property
     def get_boundaries(self):
-        return [0, len(self.labyrinth[0]), 0, len(self.labyrinth)]
+        return self._boundaries
 
+
+    def get_compass_values(self, x: int, y: int) -> [int, int, int, int]:
+        """
+        :param x:
+        :param y:
+        :return: [east, west, north, south]
+        """
+        east: int = Agent.WALL
+        west: int = Agent.WALL
+        north : int = Agent.WALL
+        south : int = Agent.WALL
+
+        if x <= self._boundaries[0]:  # out of scope on the left <= 0 - unlikely negative
+            west = -1
+            east = self.labyrinth[y][x + 1]
+        elif x >= self._boundaries[1]-1:  # out of scope on the right <= 0 - > than width of labyrinth
+            east = -1
+            west = self.labyrinth[y][x - 1]
+        else:
+            east = self.labyrinth[y][x+1]
+            west = self.labyrinth[y][x-1]
+
+        if y < self._boundaries[2]: # out of scope on first row <= 0 - unlikely negative
+            north = Agent.STONE
+            south = self.labyrinth[y + 1][x]
+        elif y >= self._boundaries[3]-1: # out of scope on last row >= no of rows.
+            south = Agent.STONE
+            north = self.labyrinth[y - 1][x]
+        else:
+            north = self.labyrinth[y-1][x]
+            south = self.labyrinth[y+1][x]
+
+        return [east, west, north, south]
+
+
+            #== y < self._boundaries[3]:
+
+    def set_cell_values(self, x: int, y: int, value: int) -> None:
+        if self._boundaries[0] <= x < self._boundaries[1]:
+            if self._boundaries[2] <= y < self._boundaries[3]:
+                self.labyrinth[y][x] = value
--- New_labyrinth/labyrinth_solver/solver.py
+++ New_labyrinth/labyrinth_solver/solver.py
@@ -1,12 +1,13 @@
 import numpy as np
 import copy
-from labyrinth_solver.agent import  Agent
-from labyrinth_solver.labyrinth import  Labyrinth
+from labyrinth_solver.agent import Agent
+from labyrinth_solver.labyrinth import Labyrinth
 
 
 class Solver:
     def __init__(self, labyrinth) -> None:
         self.labyrinth = labyrinth
+        self.labyrinth_copy = labyrinth
 
         # You may edit the following variables to help you solve the maze.
 
@@ -24,8 +25,6 @@ class Solver:
 
         self.solution = [[], []]
 
-
-
     def parse(self) -> None:
         """
         Parses the labyrinth into data structures which may help in solving the maze.
@@ -36,9 +35,9 @@ class Solver:
         print("transversable:", self.traversable)
         print(self.labyrinth)
 
-        # set the agent traversing the labyringth
-        #self.agent = Agent(0, 0, 0, 0,  len(self.labyrinth[0])-1, len(self.labyrinth)-1)
-        pass
+        self.labyrinth_copy = self._clean()
+
+
 
     @property
     def solve(self) -> bool:
@@ -49,15 +48,31 @@ class Solver:
         and no path intersects with a wall.
         A path is an ordered list of adjacent traversable points.
         """
-        labyrinth = Labyrinth(copy.deepcopy(self.labyrinth))
-        boundaries = labyrinth.get_boundaries
-        print(boundaries)
-        agent = Agent(15,-18)
-        print(agent.x)
-        print(agent.y)
-        agent.adjust(boundaries)
-        print(agent.x)
-        print(agent.y)
+        self.labyrinth_copy = self._clean()
+        for i in range(0, len(self.entry_points)):
+            labyrinth = Labyrinth(copy.deepcopy(self.labyrinth_copy), self.entry_points[i], self.exit_points[i])
+            labyrinth.set_cell_values(self.entry_points[i][0], self.entry_points[i][1], Agent.STARTING_POINT)
+            print(labyrinth.labyrinth)
+            self._find_path(labyrinth)
 
         return False
+
+    def _clean(self):
+        labyrinth_copy = copy.deepcopy(self.labyrinth)
+        for entry_points in self.entry_points:
+            labyrinth_copy[entry_points[1]][entry_points[0]] = -2
+        return labyrinth_copy
+
+    def _find_path(self, labyrinth: Labyrinth) -> object:
+
+        agent = Agent(labyrinth.entry_points[0], labyrinth.entry_points[1])
+        print(labyrinth.get_compass_values(1, 1))
+        print(labyrinth.labyrinth)
+        print(labyrinth.get_compass_values(0, 1))
+        print(labyrinth.labyrinth)
+        print(labyrinth.get_compass_values(14,1))
+        print(labyrinth.labyrinth)
+        print(labyrinth.get_compass_values(1, 14))
+        print(labyrinth.labyrinth)
+
 
