e32d6f26995a0dbd401744157fde3abf8fcd0f0c
Patricia Ryser-Welch <patricia.ryser-welch@newcastle.ac.uk>
2022-07-22 08:34:19 +0100

    both in lab

--- New_labyrinth/labyrinth_solver/solver.py
+++ New_labyrinth/labyrinth_solver/solver.py
@@ -10,10 +10,12 @@ class Solver:
     NORTH = 2
     SOUTH = 3
 
+
     def __init__(self, alabyrinth) -> None:
         self.labyrinth = alabyrinth
-        self.labyrinth_copy = []
         self.solution = [[], []]
+        self.lab_min: Labyrinth = None
+        self.lab_theseus: Labyrinth = None
 
 
         # You may edit the following variables to help you solve the maze.
@@ -30,8 +32,13 @@ class Solver:
         """
         Parses the labyrinth into data structures which may help in solving the maze.
         """
-        self.labyrinth_copy = self._clean()
-        pass
+
+        if len(self.entry_points) >= 2:
+            self.lab_theseus = Labyrinth(self._clean(), self.entry_points[0], self.exit_points[0])
+            self.lab_theseus.set_cell_values(self.entry_points[0][0], self.entry_points[0][1], Labyrinth.STARTING_POINT)
+
+            self.lab_min = Labyrinth(self._clean(), self.entry_points[1], self.exit_points[1])
+            self.lab_min.set_cell_values(self.entry_points[1][0], self.entry_points[1][1], Labyrinth.STARTING_POINT)
 
     @property
     def solve(self) -> bool:
@@ -43,25 +50,31 @@ class Solver:
         A path is an ordered list of adjacent traversable points.
         """
         solution_exists = True
+
         # create labyrinth for a entry point
 
+        if not self.lab_theseus == None and not self.lab_min == None:
+            solution_exists = self._find_path()
+            print(self.lab_min.labyrinth)
+            print(self.lab_theseus.labyrinth)
 
-        for i in range(0, len(self.entry_points)):
+
+        #for i in range(0, len(self.entry_points)):
             # create labyrinth for a entry point
-            labyrinth = Labyrinth(copy.deepcopy(self.labyrinth_copy), self.entry_points[i], self.exit_points[i])
+            #labyrinth = Labyrinth(copy.deepcopy(self.labyrinth_copy), self.entry_points[i], self.exit_points[i])
             # add entry point
-            labyrinth.set_cell_values(self.entry_points[i][0], self.entry_points[i][1], Labyrinth.STARTING_POINT)
-            print(" --- prior: ---")
-            print(labyrinth.labyrinth)
+            #labyrinth.
+
+           # print(" --- prior: ---")
+           # print(labyrinth.labyrinth)
             # solve and retried a solution
-            solution = self._find_path(labyrinth)
-            self.solution[i] = solution[1]
-            solution_exists = solution_exists & solution[0]
-        
-            print("---after ----")
-            self.entry_points[i]
-            print(labyrinth.labyrinth)
-            print("----")
+           # solution = self._find_path(labyrinth)
+           # self.solution[i] = solution[1]
+           # solution_exists = solution_exists & solution[0]
+           # print("---after ----")
+           # self.entry_points[i]
+           # print(labyrinth.labyrinth)
+           # print("----")
 
         return solution_exists
 
@@ -71,16 +84,41 @@ class Solver:
             labyrinth_copy[entry_points[1]][entry_points[0]] = -2
         return labyrinth_copy
 
-    def _find_path(self, labyrinth: Labyrinth) -> [bool,[]]:
-        agent = Agent(labyrinth.entry_points[0], labyrinth.entry_points[1], labyrinth.get_boundaries)
+    def _find_path(self) -> bool:
+        theseus = Agent(self.lab_theseus.entry_points[0],
+                        self.lab_theseus.entry_points[1],
+                        self.lab_theseus.get_boundaries)
+        minautor = Agent(self.lab_min.entry_points[0],
+                         self.lab_min.entry_points[1],
+                         self.lab_min.get_boundaries)
+        both_out = self.lab_theseus.found_exit_point(theseus.x, theseus.y) and \
+                   self.lab_min.found_exit_point(minautor.x, minautor.y)
 
-        while not labyrinth.found_exit_point(agent.x, agent.y):
-            # Drop stone in current location in the labyrinth
-            labyrinth.set_cell_values(agent.x, agent.y, Agent.STONE)
-            # use a compass to find information on current location surroundings
-            values = labyrinth.get_compass_values(agent.x, agent.y)
-            # move
-            agent.move(values[Solver.EAST], values[Solver.WEST], values[Solver.NORTH], values[Solver.SOUTH])
+        while not both_out:
+            if not self.lab_theseus.found_exit_point(theseus.x, theseus.y):
+                self.lab_theseus.set_cell_values(theseus.x, theseus.y, Agent.STONE)
+                values = self.lab_theseus.get_compass_values(theseus.x, theseus.y)
+                theseus.move(values[Solver.EAST],
+                             values[Solver.WEST],
+                             values[Solver.NORTH],
+                             values[Solver.SOUTH])
+                #print(self.lab_theseus.labyrinth)
 
-        return [labyrinth.found_exit_point(agent.x, agent.y), agent.path]
+            if not self.lab_min.found_exit_point(minautor.x, minautor.y):
+                self.lab_min.set_cell_values(minautor.x, minautor.y, Agent.STONE)
+                values = self.lab_min.get_compass_values(minautor.x, minautor.y)
+                minautor.move(values[Solver.EAST],
+                              values[Solver.WEST],
+                              values[Solver.NORTH],
+                              values[Solver.SOUTH])
+                #print(self.lab_min.labyrinth)
+
+            both_out = self.lab_theseus.found_exit_point(theseus.x, theseus.y) and \
+                       self.lab_min.found_exit_point(minautor.x, minautor.y)
+            print(both_out)
+
+        self.solution[0] = theseus.path
+        self.solution[1] = minautor.path
+        return both_out
+
 
